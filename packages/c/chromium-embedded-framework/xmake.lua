764d503a49af3e93a1006b6ebdef485d6ee02843
Lorenzo Lapucci <lorelap@amazon.com>
2025-03-30 11:47:19 +0100

    Update MacOS version and setup

--- packages/c/chromium-embedded-framework/xmake.lua
+++ packages/c/chromium-embedded-framework/xmake.lua
@@ -5,7 +5,6 @@ package("chromium-embedded-framework")
     set_license("BSD-3-Clause")
 
     local buildver = {
-        ["131.3.4"] = "131.3.4+g7ecebf0+chromium-131.0.6778.140",
         ["134.3.2"] = "134.3.2+g615db2f+chromium-134.0.6998.89"
     }
   
@@ -17,12 +16,12 @@ package("chromium-embedded-framework")
             add_versions("134.3.2", "856cccd8f8b7ebd4cabad7a7ce1bd7596c18bba641bb0f2eae4d3ee51b3c7265")
         end
         add_configs("runtime", {description = "Set vs compiler runtime.", default = "MT", type = "string", readonly = true})
-    elseif is_plat("macos") then
+    elseif is_plat("macosx") then
         add_urls("https://cef-builds.spotifycdn.com/cef_binary_$(version).tar.bz2", {version = function (version)
             return format("%s_macos%s", buildver[tostring(version)], (is_arch("x64") and "x64" or "arm64"))
         end})
         if is_arch("arm64") then
-            add_versions("131.3.4", "c16329ff3beff7ab383d93fe5785eafe83dc3c0b34992cf28c59b0aa3f6fc7ec")
+            add_versions("134.3.2", "dd6551579493ee203d70ddaccc243c87cd5151f956e49203925992adfd3c3dcd")
         end
     end
 
@@ -43,13 +42,9 @@ package("chromium-embedded-framework")
         import("package.tools.xmake").install(package)
     end)
 
-    on_install("macos", function (package)
+    on_install("macosx", function (package)
         local distrib_type = package:debug() and "Debug" or "Release"
-        os.cp(path.join(distrib_type, "*.lib"), package:installdir("lib"))
-        os.cp(path.join(distrib_type, "*.dll"), package:installdir("bin"))
-        os.cp(path.join(distrib_type, "swiftshader", "*.dll"), package:installdir("bin", "swiftshader"))
-        os.cp(path.join(distrib_type, "*.bin"), package:installdir("bin"))
-        os.cp("Resources/*", package:installdir("bin"))
+        os.cp(distrib_type, package:installdir("bin"))
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         import("package.tools.xmake").install(package)
     end)
